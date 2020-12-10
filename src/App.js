e86d04df33cb6b88ed6fae74e9d60cd6a2e03d0a
pgoytia86 <67652733+pgoytia86@users.noreply.github.com>
2020-12-10 11:06:32 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ import HomeScreen from "./screens/HomeScreen";
 import AdminScreen from "./screens/AdminScreen";
 
 
+
+import {Grid, IconButton} from '@material-ui/core';
+
+import InstagramIcon from '@material-ui/icons/Instagram';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import WebIcon from '@material-ui/icons/Web';
+
+
+
 class App extends React.Component {
  
   render() {
@@ -18,6 +27,28 @@ class App extends React.Component {
       <div className="grid-container">
         <header>
           <Link to="/">ZAZUSHOWROOM | Shopping Cart</Link>
+      <Grid item >
+                    <IconButton >
+                        <InstagramIcon />
+                        <a href="https://www.instagram.com/zazu.s" target="_blank" rel="noopener noreferrer">
+                        Ig
+                        </a>
+                    </IconButton>                   
+                        <IconButton >
+                         <FacebookIcon />
+                         <a href="https://www.facebook.com/zazu.showroom" target="_blank" rel="noopener noreferrer">
+                            Fb
+                            </a>
+                         </IconButton>
+
+                         <IconButton >
+                    <WebIcon/>
+                    <a href="https://www.zazushowroom.com" target="_blank" rel="noopener noreferrer">
+                        Web
+                        </a>
+                     </IconButton>                  
+                </Grid>
+                
           <Link to="/admin" > Admin  </Link>
         
         </header>
